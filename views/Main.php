2ffaf3b24cba7694a18b87e1a5b30a78b2967f67
Allan Collins <collinsinternet@gmail.com>
2021-04-12 14:24:46 -0600

    Init

--- views/Main.php
+++ views/Main.php
@@ -2,12 +2,5 @@
 	<h2>Migrator</h2>
     <hr>
     <p>Migrate your site now!</p>
-    <pre>
-    <?php
-    $square = new \Migrator\Square\Square_Space();
-    $square->get_sitemap();
 
-
-    ?>
-    </pre>
 </div>
